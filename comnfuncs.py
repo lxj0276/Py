f0c0fd25b5c8e362ac42a2531377ef862f2007be
tober <zhyiuwai@outlook.com>
2018-04-13 14:26:36 +0800

    公用函数

---
+++ comnfuncs.py
@@ -0,0 +1,23 @@
+def clear(arr, d=2):
+    brr = arr.copy()
+    for i in range(len(arr)):
+        brr[i] = round(arr[i], d)
+    brr[brr.argmax()] = brr[brr.argmax()] + (1.0 - brr.sum())
+
+    return brr
+
+
+def indics(z):
+    N = len(z)
+    rtn = (z[-1] / z[0]) ** (12 / N) - 1
+    vol = z.pct_change().std() * (12 ** 0.5)
+    sp = (rtn - 0.04) / vol
+    dd = []
+    for i in range(N):
+        Z = z[:i+1]
+        dd.append(1 - Z[-1] / Z.max())
+    dd_max = max(dd)
+    cm = rtn / dd_max
+    print(rtn, vol, sp, dd_max, cm)
+
+    return rtn, vol, sp, dd_max, cm
