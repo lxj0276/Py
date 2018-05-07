dd08ccab5c0b6024ae461cf6bc3b6259e65bc695
tober <zhyiuwai@outlook.com>
2018-05-07 10:56:22 +0800

    完成系统beta函数，并重命名文件

---
+++ strategy_beta.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+"""
+投资组合优化函数
+如果资产数目为n，则
+
+Parameters
+----------
+r : array of shape (n)
+    收益向量
+sigma : array of shape (n)
+    方差向量
+Sigma : array of shape (n, n)
+    协方差矩阵
+Rho: array of shape (n, n)
+    相关系数矩阵   
+
+Returns
+-------
+w* : array of shape (n)
+    最优权重向量  
+"""
+
+
+import numpy as np
+from scipy.optimize import minimize
+
+
+def optimize(func, *args):
+
+    N = len(args[0])
+    cons = ({'type': 'eq',
+             'fun': lambda x: np.array(x.sum() - 1.0),
+             'jac': lambda x: np.ones(N)})
+    res = minimize(func, [1/N]*N, args=args, constraints=cons, bounds=[(0, 1)] * N,
+                   method='SLSQP', tol=1e-18, options={'disp': False, 'maxiter': 1000})
+
+    return res.x
+
+
+def max_sharp(r, Sigma):
+    ''' 最大夏普比
+
+    Parameters
+    ----------
+    r : array of shape (n)
+        收益向量
+    Sigma : array of shape (n)
+        协方差矩阵 
+
+    Returns
+    -------
+    w* : array of shape (n)
+        最优权重向量   
+    '''
+
+    def func(x, r, Sigma, sign=-1.0):
+        res = x.dot(r) / (x.dot(Sigma).dot(x)) ** 0.5
+        return res * sign
+
+    return optimize(func, r, Sigma)
+
+
+def min_vol(Sigma):
+    ''' 最小波动率
+
+    Parameters
+    ----------
+    Sigma : array of shape (n)
+        协方差矩阵
+
+
+    Returns
+    -------
+    w* : array of shape (n)
+        最优权重向量   
+    '''
+
+    def func(x, Sigma, sign=1.0):
+        res = (x.dot(Sigma).dot(x)) ** 0.5
+        return res * sign
+
+    return optimize(func, Sigma)
+
+
+def vol_parity(sigma):
+    ''' 波动率平价    
+
+    Parameters
+    ----------
+    sigma : array of shape (n)
+        方差向量
+
+    Returns
+    -------
+    w* : array of shape (n)
+        最优权重向量   
+    '''
+
+    return (1.0 / sigma) / (1.0 / sigma).sum()
+
+
+def risk_parity(Sigma):
+    ''' 风险平价    
+
+    Parameters
+    ----------
+    Sigma : array of shape (n, n)
+        协方差矩阵
+
+    Returns
+    -------
+    w* : array of shape (n)
+        最优权重向量   
+    '''
+
+    def func(x, Sigma, sign=1.0):
+        RC = x * Sigma.dot(x) / (x.dot(Sigma).dot(x)) ** 0.5
+        res = ((np.tile(RC, (len(RC), 1)).T - RC) ** 2).sum().sum()
+        return res * sign
+
+    return optimize(func, Sigma)
+
+
+def most_diversified(sigma, Sigma):
+    ''' 最大分散化   
+
+    Parameters
+    ----------
+    sigma : array of shape (n)
+        方差向量
+    Sigma : array of shape (n, n)
+        协方差矩阵
+
+    Returns
+    -------
+    w* : array of shape (n)
+        最优权重向量   
+    '''
+
+    def func(x, sigma, Sigma, sign=-1.0):
+        res = x.dot(sigma) / (x.dot(Sigma).dot(x)) ** 0.5
+        return res * sign
+
+    return optimize(func, sigma, Sigma)
+
+
+def most_decorr(Rho):
+    ''' 最大去相关系数    
+
+    Parameters
+    ----------
+    Rho: array of shape (n, n)
+        相关系数矩阵   
+
+    Returns
+    -------
+    w* : array of shape (n)
+        最优权重向量   
+    '''
+    def func(x, Rho, sign=1.0):
+        res = x.dot(Rho).dot(x)
+        return res * sign
+
+    return optimize(func, Rho)
+
+
+def max_entropy(Sigma, k=2):
+    ''' 最大熵   
+
+    Parameters
+    ----------
+    Sigma : array of shape (n, n)
+        协方差矩阵
+    k : int
+        约束条件个数
+
+    Returns
+    -------
+    w* : array of shape (n)
+        最优权重向量   
+    '''
+
+    def safe_log(arr):
+        brr = np.zeros_like(arr)
+        brr = np.where(arr > 0, np.log(arr), 0)
+        return brr
+
+    def func(x, Sigma, sign=1.0):
+        Sigma = (Sigma + Sigma.T) / 2.0
+        eig_values, eig_vectors = np.linalg.eig(Sigma)
+        
+        descend_order = eig_values.argsort()[::-1]
+        a = eig_values[descend_order]
+        b = eig_vectors[:, descend_order]
+        
+        y = b.T.dot(x)
+        v = (y**2) * a
+        p = v / v.sum()
+        res = (p * safe_log(p))[k:].sum()
+        
+        return res * sign
+
+    return optimize(func, Sigma)
