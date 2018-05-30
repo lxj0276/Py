f8cf8617f72e57aec47e0b5440949a955689620d
tober <zhyiuwai@outlook.com>
2018-05-30 10:43:01 +0800

    添加注释

--- rule_based.py
+++ rule_based.py
@@ -3,6 +3,19 @@ from scipy.stats import norm
 
 
 def stop_loss(sr_ret, r_stop_loss, r_re_entry):
+    """
+    止损
+    
+    Parameters
+    ----------
+    sr_ret: Series, 收益率序列
+    r_stop_loss: float, 止损线
+    r_re_entry: float, 入场线
+    
+    Returns
+    -------
+    sr_pos: Series, 仓位序列
+    """
     def strategy(pos, ret):
         if ret > r_re_entry:
             return 1
@@ -20,6 +33,20 @@ def stop_loss(sr_ret, r_stop_loss, r_re_entry):
 
 
 def target_vol(sr_sigma, tar_sigma, MaxExp, tol_change):
+    """
+    目标波动率
+    
+    Parameters
+    ----------
+    sr_sigma: Series, 波动率序列
+    tar_sigma: float, 目标波动率
+    MaxExp: float, 风险敞口上限
+    tol_change： float, 仓位变动幅度
+        
+    Returns
+    -------
+    sr_pos: Series, 仓位序列
+    """
     def strategy(pos, sigma):
         tar_w = min(tar_sigma / sigma, MaxExp)
             if (1 - tol_change) < (pos / tar_w) < (1 + tol_change):
@@ -32,14 +59,41 @@ def target_vol(sr_sigma, tar_sigma, MaxExp, tol_change):
     for i in range(1, len(sr_sigma)):
         sr_pos[i] = strategy(sr_pos[i-1], sr_sigma[i])
 
-    return sr_pos.shift(1)
+    return sr_pos
 
 
-def dd_control(sr_CVaR, tar_CVaR, MaxExp, tol_change):
-    return target_vol(sr_CVaR, tar_CVaR, MaxExp, tol_change)
+def dd_control(sr_CVaR, tar_CVaR, MaxExp):
+    """
+    回撤控制
+    
+    Parameters
+    ----------
+    sr_CVaR: Series, CVaR序列
+    tar_CVaR: float, 目标CVaR
+    MaxExp: float, 风险敞口上限
+        
+    Returns
+    -------
+    sr_pos: Series, 仓位序列
+    """
+    return (tar_CVaR / sr_CVaR).clip_upper(MaxExp)
 
 
 def CPPI(sr_ret_c, sr_ret_f, m, f0):
+    """
+    CPPI
+    
+    Parameters
+    ----------
+    sr_ret_c: Series, 风险资产收益序列
+    sr_ret_f: Series, 无风险资产收益序列
+    m: 乘数
+    f0: float, 无风险资产初始比例
+        
+    Returns
+    -------
+    sr_ratio_c: Series, 风险资产仓位序列
+    """
 
     def net_value(value, ret_c, ret_f):
         ratio_c = (value - f0) * m / (f0 + (value - f0) * m)
@@ -53,11 +107,24 @@ def CPPI(sr_ret_c, sr_ret_f, m, f0):
         sr_ratio_c[i], sr_net_value[i] = net_value(
             sr_net_value[i-1], sr_ret_c[i-1], sr_ret_f[i-1])
 
-    return sr_ratio
+    return sr_ratio_c
 
 
-def CPPI(sr_ret_c, sr_ret_f, m, alpha):
+def TIPP(sr_ret_c, sr_ret_f, m, alpha):
+    """
+    复制型TIPP
+    
+    Parameters
+    ----------
+    sr_ret_c: Series, 风险资产收益序列
+    sr_ret_f: Series, 无风险资产收益序列
+    m: float, 乘数
+    alpha: float, 保本比例
 
+    Returns
+    -------
+    sr_ratio_c: Series, 风险资产仓位序列
+    """
     def net_value(value, ret_c, ret_f):
         ratio_c = value * (1 - alpha) * m / \
             (value * (1 - alpha) * m + value * alpha)
@@ -75,17 +142,61 @@ def CPPI(sr_ret_c, sr_ret_f, m, alpha):
 
 
 def OBPI(sr_p_a, sr_p_b, sr_v_a, sr_v_b, t):
+    """
+    复制型OBPI
+    
+    Parameters
+    ----------
+    sr_p_a: Series, a资产价格序列
+    sr_p_b: Series, b资产价格序列
+    sr_v_a: Series, a资产波动率序列
+    sr_v_b: Series, b资产波动率序列
+    t: float, 时间
+
+    Returns
+    -------
+    sr_pos: Series, 仓位序列
+    """
     sr_d1 = (np.log(sr_p_a / sr_p_b) + (sr_v_a - sr_v_b)
              ** 2 * t / 2) / ((sr_v_a - sr_v_b) * t ** 0.5)
 
     return sr_d1.apply(lambda d1: norm.cdf(d1))
 
 
-def VaRcover(sr_rf, sr_VaR, p):
+def VaRcover(sr_VaR, rf, p):
+    """
+    VaR套补
+    
+    Parameters
+    ----------
+    sr_VaR: Series, VaR序列
+    rf: float, 无风险收益率
+    p: flota, 套补比例
+
+    Returns
+    -------
+    sr_pos: Series, 仓位序列
+    """
+
     return 1 / (p * sr_VaR / sr_rf + 1)
 
 
 def margrabe(sr_p_a, sr_p_b, sr_v_a, sr_v_b, t):
+    """
+    margrabe资产交换
+    
+    Parameters
+    ----------
+    sr_p_a: Series, a资产价格序列
+    sr_p_b: Series, b资产价格序列
+    sr_v_a: Series, a资产波动率序列
+    sr_v_b: Series, b资产波动率序列
+    t: float, 时间
+
+    Returns
+    -------
+    sr_pos: Series, a资产仓位序列
+    """
     sr_pos = sr_p_a.copy()
     for i in range(len(sr_p_a)):
         d1 = (np.log(sr_p_a[i] / sr_p_b[i]) + (sr_v_a[i] - sr_v_b[i])
