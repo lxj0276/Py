b2473a715e183501dec51e21896d0300d2f852e8
zhangyw <zhyiuwai@gmail.com>
2018-08-09 15:45:35 +0800

    arp start

---
+++ arp.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  9 15:25:49 2018
+
+@author: zhangyw49
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+# 股票和债券指数数据
+## 股票指数50 300 500 1000 行业指数 债券指数 国债 金融债 企业债
+
+# 价值函数
+
+# 动量函数
+
+# VOl函数
+
+# Rev函数
+
+# 多空处理函数
+
+
+# 最后构建组合
+
+# 如何更新数据？关键在于溢价因子表现跟踪
+
+
