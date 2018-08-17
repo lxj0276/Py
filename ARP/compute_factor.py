be8e4d88dae158211c7368b33da915e82152c35d
tober <zhyiuwai@outlook.com>
2018-08-17 13:03:13 +0800

    模块化

---
+++ ARP/compute_factor.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 17 10:16:00 2018
+
+@author: zhangyw49
+"""
+
+import numpy as np
+import pandas as pd
+
+def factor_to_positon(df_factor, positions):
+    df_rank = df_factor.rank(axis=1, ascending=False)
+    df_position = df_rank.applymap(lambda x: positions[int(x)-1])
+    return df_position
+    
+
+# 股票mom算法
+## 计算逻辑：最近五日均值与历史L天最大值之比，最后再取平均
+def stock_mom_factor(df, m, lags):
+    def mom(lag):
+        return df.rolling(m).mean() / df.rolling(lag, min_periods=10).max().shift(1) - 1
+    df_factor = mom(lags[0]) + mom(lags[1]) + mom(lags[2])
+    return df_factor
+
+def stock_mom(df, m=5, lags=[210, 240, 270], positions=[0.5, 0.5, -0.5, -0.5]):
+    df_factor = stock_mom_factor(df, m, lags)
+    df_position = factor_to_positon(df_factor, positions)
+    df_price = df.copy()
+    return df_price, df_position
+
+
+# 股票vol算法
+## 计算逻辑：最近五日波动率均值与L天之前的五日均值之比，最后取平均
+def stock_vol_factor(df, m, lags):
+    log_rtn = np.log(df).diff(1)
+    std = log_rtn.expanding().std()
+    def vol(lag):
+        return -std.rolling(m).mean() / std.rolling(m).mean().shift(lag)
+    df_factor = vol(lags[0]) + vol(lags[1]) + vol(lags[2])
+    return df_factor
+
+def stock_vol(df, m=5, lags=[210, 240, 270], positions=[0.5, 0.5, -0.5, -0.5]):
+    df_factor = stock_vol_factor(df, m, lags)
+    df_position = factor_to_positon(df_factor, positions)
+    df_price = df.copy()
+    return df_price, df_position
+
+
+# 债券csm算法
+## 计算逻辑：L天的收益率，取平均
+def bond_csm_factor(df, lags):
+    def csm(lag):
+        return df.pct_change(lag)
+    df_factor = (csm(lags[0]) + csm(lags[1]) + csm(lags[2])).shift(1)
+    return df_factor
+
+def bond_csm(df, lags=[20, 40, 60], positions=[0.5, 0.5, -0.5, -0.5]):
+    df_factor = bond_csm_factor(df, lags)
+    df_position = factor_to_positon(df_factor, positions)
+    df_price = df.copy()
+    return df_price, df_position
+
+
+# 债券tsm算法
+## 计算逻辑：构造DR007指数，计算三个周期的ts动量
+def bond_tsm_factor(df, df_rf, lags):
+    df_rf.columns = ["rf"]
+    df_bind = pd.concat([df, df_rf], axis=1)
+    df_bind = df_bind.fillna(method="ffill")
+    def tsm(lag):
+        return df_bind.pct_change(lag)
+    df_tsm = (tsm(lags[0]) + tsm(lags[1]) + tsm(lags[2])).shift(1)
+    return df_bind, df_tsm
+
+def bond_tsm_position(df_tsm):
+    df = df_tsm.subtract(df_tsm.rf, axis=0)
+    def position(x):
+        if x<= 0:
+            return 0
+        elif x > 0:
+            return 1
+        else:
+            return np.nan
+    df = df.applymap(lambda x: position(x))
+    n = df.shape[1] - 1
+    df["rf"] = n - df.sum(axis=1)
+    df = df / n
+    return df
+
+def bond_tsm(df, df_rf, lags=[20, 40, 60]):
+    df_price, df_factor = bond_tsm_factor(df, df_rf, lags)
+    df_position = bond_tsm_position(df_factor)
+    return df_price, df_position
+
+# 债券value算法
+## 计算逻辑：YTM五日平均，分别取50，100，150日最大值的比率平均
+def bond_value_ytm(df, ytm_origin, transform_matrix):
+    ytm_transform = ytm_origin.dot(transform_matrix)
+    ytm_transform.columns = df.columns
+    return ytm_transform 
+
+def bond_value_factor(ytm_transform, m, lags):
+    df_avg = ytm_transform.rolling(m).mean()
+    def value(lag):
+        df = df_avg / df_avg.rolling(lag).max()
+        return df
+    df_value = value(lags[0]) + value(lags[1]) + value(lags[2])
+    return df_value
+
+def bond_value(df, df_ytm, transform_matrix, m=5, lags=[50, 100, 150]):
+    ytm_transform = bond_value_ytm(df, df_ytm, transform_matrix)
+    df_factor = bond_value_factor(ytm_transform, m, lags)
+    df_price = df.copy()
+    return df_price, df_factor
+
+
+# 债券carry算法
+# 1.首先，五个国债指数及其收益
+# 2.需要数据，2，4，6，8，9，10 20 以及DR007 的YTM
+# 3.YTM滚动10日平均
+# 4. YTM对对应期限的斜率
+# 5. 对斜率， 100 * （最近60天 - 之前750天），以及500天和250天
+# 6. 对上述求均值。并称之为套索
+# 7. 套索为正，买长，套索为负，买短
+def bond_carry_ytm(df, df_rf, ytm_origin, transform_matrix):
+    ytm_transform = ytm_origin.dot(transform_matrix)
+    ytm_transform.columns = df.columns
+    df_rf.columns = ['rf']
+    ytm_transform = pd.concat([df_rf, ytm_transform], axis=1)
+    ytm_transform.fillna(method="ffill", inplace=True)
+    return ytm_transform
+
+def bond_carry_facotr(ytm_transform, m, x, lags):
+    def slope(x, y):
+        x_centered = x - np.mean(x)
+        y_centered = y - np.mean(y)
+        slope = np.dot(x_centered, y_centered) / np.dot(x_centered, x_centered)
+        return slope
+    df_slope = ytm_transform.apply(lambda y: slope(x, y), axis=1)
+    
+    def carry(lag):
+        df = (df_slope.rolling(m).mean() - df_slope.shift(lag).rolling(m).mean()) * 100
+        return df
+    df_carry = carry(lags[0]) + carry(lags[1]) + carry(lags[2]) 
+    return df_carry
+
+def bond_carry_position(df, df_carry, positions):
+    df_ones = pd.DataFrame(np.ones((df.shape[0], df.shape[1])), index=df.index, columns=df.columns)
+    df_position = df_ones * positions
+    df_position = np.where(df_carry>0, df_position, -1 * df_position)
+    return df_position
+
+def bond_carry(df, df_rf, ytm_origin, transform_matrix, positions, m=60, x=[7/365, 2, 4, 6, 17/2, 50/3], lags=[250, 500, 750]):
+    ytm_transform = bond_carry_ytm(df, df_rf, ytm_origin, transform_matrix)
+    df_factor = bond_carry_facotr(ytm_transform, m, x, lags)
+    df_position = bond_carry_position(df, df_carry, positions)
+    df_price = df.copy()
+    return df_price, df_position
+
+
+# 跨类rev算法
+## 计算逻辑：计算对数收益率，某段历史正收益之和与负收益之和的比值，取负，取对数，再取负
+## 简化之后，负收益之和与正收益之和的比值，取绝对值，取对数
+def asset_rev(df, lags=[240, 300, 360]):
+    log_rtn = np.log(df).diff(1)
+    def rev(lag):
+        return log_rtn.rolling(lag).apply(lambda x: np.log(-np.sum(x[x<0]) / np.sum(x[x>0])), raw=True)
+    return rev(lags[0]) + rev(lags[1]) + rev(lags[2])
+
+
+# 跨类vol算法
+## 计算逻辑：对数收益率，三段标准差的均值，三段区间标准差的变化率，再取均值
+def asset_vol(df, lags=[60, 120, 180]):
+    log_rtn = np.log(df).diff(1)
+    def std(lag):
+        return log_rtn.rolling(lag).std()
+    std = std(lags[0]) + std(lags[1]) + std(lags[2])
+    def vol(lag):
+        return std.diff(lag)
+    return vol(lags[0]) + vol(lags[1]) + vol(lags[2])
+
+
+# 跨类value算法
+## 计算逻辑：对于两个股票指数，100 / pe, pb, ps, 最近五日平均，历史所有score, 再取平均，对于债券指数，YTM五日平均，取历史所有score##
+
+def asset_value_zscore(df, m):
+    roll = df.rolling(m).mean()
+    mean = roll.expanding().mean()
+    std = roll.expanding().std()
+    zscore = (roll - mean) / std
+    return zscore
+
+def asset_value(df, df_value_stock, df_value_bond, m=5):
+    df_stock = asset_value_zscore(100 / df_value_stock, m)
+    df_stock = df_stock.mean(axis=1, level=0)
+    df_bond = asset_value_zscore(df_value_bond, m)
+    df_value = pd.concat([df_stock, df_bond], axis=1)
+    df_value.columns = df.columns
+    return df_value
--- ARP/parameters.py
+++ ARP/parameters.py
@@ -14,30 +14,97 @@ plt.rcParams['axes.unicode_minus'] = False
 
 # 股票规模指数：上证50 沪深300 中证500 中证1000
 stock_size_index = ['000016.SH', '000300.SH', '000852.SH', '000905.SH']
-# 股票行业申万指数：农林牧渔 采掘 化工 钢铁 有色金属 电子 家用电器 食品饮料 纺织服装 轻工制造 医药生物 公用事业 交通运输 房地产商业贸易 休闲服务 综合 建筑材料 建筑装饰 电气设备 国防军工 计算机传媒 通信 银行 非银金融 汽车 机械设备
-stock_sector_index = ['801010.SI', '801020.SI', '801030.SI', '801040.SI', '801050.SI', '801080.SI', '801110.SI', '801120.SI', '801130.SI', '801140.SI', '801150.SI', '801160.SI', '801170.SI',
-                      '801180.SI', '801200.SI', '801210.SI', '801230.SI', '801710.SI', '801720.SI', '801730.SI', '801740.SI', '801750.SI', '801760.SI', '801770.SI', '801780.SI', '801790.SI', '801880.SI', '801890.SI']
+# 股票行业指数(申万一级)：农林牧渔 采掘 化工 钢铁 有色金属 电子 家用电器 食品饮料 
+# 纺织服装 轻工制造 医药生物 公用事业 交通运输 房地产商业贸易 休闲服务 综合 
+# 建筑材料 建筑装饰 电气设备 国防军工 计算机传媒 通信 银行 非银金融 汽车 机械设备
+stock_sector_index = ['801010.SI', '801020.SI', '801030.SI', '801040.SI', 
+                      '801050.SI', '801080.SI', '801110.SI', '801120.SI', 
+                      '801130.SI', '801140.SI', '801150.SI', '801160.SI', 
+                      '801170.SI', '801180.SI', '801200.SI', '801210.SI', 
+                      '801230.SI', '801710.SI', '801720.SI', '801730.SI', 
+                      '801740.SI', '801750.SI', '801760.SI', '801770.SI', 
+                      '801780.SI', '801790.SI', '801880.SI', '801890.SI']
 
-# 国债财富指数：中债-国债总财富(1-3年)指数   中债-国债总财富(3-5年)指数    中债-国债总财富(5-7年)指数    中债-国债总财富(7-10年)指数   中债-国债总财富(10年以上)指数
-bond_treasury_index = ['CBA00621.CS', 'CBA00631.CS', 'CBA00641.CS', 'CBA00651.CS', 'CBA00661.CS']
-# 金融债财富指数：中债-金融债券总财富(1-3年)指数   中债-金融债券总财富(3-5年)指数  中债-金融债券总财富(5-7年)指数  中债-金融债券总财富(7-10年)指数 中债-金融债券总财富(10年以上)指数
-bond_finance_index = ['CBA01221.CS', 'CBA01231.CS', 'CBA01241.CS', 'CBA01251.CS', 'CBA01261.CS']
-# AAA企业债财富指数：中债-企业债AAA财富(1-3年)指数   中债-企业债AAA财富(3-5年)指数 中债-企业债AAA财富(5-7年)指数 中债-企业债AAA财富(7-10年)指数    中债-企业债AAA财富(10年以上)指数
-bond_corporate_index = ['CBA04221.CS', 'CBA04231.CS', 'CBA04241.CS', 'CBA04251.CS', 'CBA04261.CS']
+# 国债财富指数：中债-国债总财富(1-3年)指数   中债-国债总财富(3-5年)指数    
+# 中债-国债总财富(5-7年)指数    中债-国债总财富(7-10年)指数   中债-国债总财富(10年以上)指数
+bond_treasury_index = ['CBA00621.CS', 'CBA00631.CS',
+                       'CBA00641.CS', 'CBA00651.CS', 'CBA00661.CS']
+# 金融债财富指数：中债-金融债券总财富(1-3年)指数   中债-金融债券总财富(3-5年)指数  
+# 中债-金融债券总财富(5-7年)指数  中债-金融债券总财富(7-10年)指数 中债-金融债券总财富(10年以上)指数
+bond_finance_index = ['CBA01221.CS', 'CBA01231.CS',
+                      'CBA01241.CS', 'CBA01251.CS', 'CBA01261.CS']
+# AAA企业债财富指数：中债-企业债AAA财富(1-3年)指数   中债-企业债AAA财富(3-5年)指数 
+# 中债-企业债AAA财富(5-7年)指数 中债-企业债AAA财富(7-10年)指数    中债-企业债AAA财富(10年以上)指数
+bond_corporate_index = ['CBA04221.CS', 'CBA04231.CS',
+                        'CBA04241.CS', 'CBA04251.CS', 'CBA04261.CS']
 
-# 跨类指数：中证全指  恒生中国企业指数  中债-国债总财富(总值)指数 中债-金融债券总财富(总值)指数 中债-企业债AAA财富(总值)指数
-asset_class_index = ['000985.CSI', 'HSCEI.HI', 'CBA00601.CS', 'CBA01201.CS', 'CBA04201.CS']
+# 跨类指数：中证全指  恒生中国企业指数  中债-国债总财富(总值)指数 
+# 中债-金融债券总财富(总值)指数 中债-企业债AAA财富(总值)指数
+asset_class_index = ['000985.CSI', 'HSCEI.HI',
+                     'CBA00601.CS', 'CBA01201.CS', 'CBA04201.CS']
+
+
+# 国债YTM：2年, 4年, 6年, 8年, 9年, 10年, 20年
+bond_treasury_ytm = ["M1000159", "M1000161", "M1000163", 
+                     "M1000165", "M1004678", "M1000166", "M1000168"]
+# 国开债YTM：2年, 4年, 6年, 8年, 9年, 10年, 20年
+bond_finance_ytm = ["M1004264", "M1004266", "M1004268", 
+                    "M1004270", "M1004688", "M1004271", "M1004273"]
+# 3A企业债YTM：2年, 4年, 6年, 8年, 9年, 10年, 15年
+bond_corporate_ytm = ["M1000369", "M1000371", "M1000373", 
+                      "M1000375", "M1006943", "M1000376", "M1000377"]
+
+# 跨类资产债券YTM：国债10年   金融债5年   企业债5年
+asset_class_ytm = ["M1000162", "M1004267", "M1000372"]
+
+# 无风险利率：DR007
+rate_riskfree = ['DR007.IB']
 
 
 
 param_stock_size_mom = {"m": 5, "lag": [210, 240, 270], "position": [0.5, 0.5, -0.5, -0.5]}
+param_stock_sector_mom = {"m": 5, "lag": [210, 240, 270], "position": [1/14]*14 + [-1/14]*14}
+
+param_stock_sector_vol = {"m": 5, "lag": [240, 270, 300], "position": [1/14]*14 + [-1/14]*14}
 param_stock_size_vol = {"m": 5, "lag": [240, 270, 300], "position": [0.5, 0.5, -0.5, -0.5]}
 
-param_stock_sector_mom = {"m": 5, "lag": [210, 240, 270], "position": [1/14]*14 + [-1/14]*14}
-param_stock_sector_vol = {"m": 5, "lag": [240, 270, 300], "position": [1/14]*14 + [-1/14]*14}
 
 
-param_bond_treasury_csm = {"m": 5, "lag": [240, 270, 300], "position": [1/14]*14 + [-1/14]*14}
+
+param_bond_treasury_csm = {"lags": [20, 40, 60]}
+param_bond_finance_csm = {"lags": [20, 40, 60]}
+param_bond_corporate_csm = {"lags": [20, 40, 60]}
+
+param_bond_treasury_tsm = {"lags": [20, 40, 60]}
+param_bond_finance_tsm = {"lags": [20, 40, 60]}
+param_bond_corporate_tsm = {"lags": [20, 40, 60]}
+
+param_bond_treasury_value = {"m": 5, "lags": [50, 100, 150], "transform_matrix": [[1,0,0,0,0,0,0], [0,1,0,0,0,0,0], [0,0,1,0,0,0,0], [0,0,0,1/2,1/2,0,0], [0,0,0,0,0,1/3,2/3]]}
+param_bond_finance_value = {"m": 5, "lags": [50, 100, 150], "transform_matrix": [[1,0,0,0,0,0,0], [0,1,0,0,0,0,0], [0,0,1,0,0,0,0], [0,0,0,1/2,1/2,0,0], [0,0,0,0,0,1/3,2/3]]}
+param_bond_corporate_value = {"m": 5, "lags": [50, 100, 150], "transform_matrix": [[1,0,0,0,0,0,0], [0,1,0,0,0,0,0], [0,0,1,0,0,0,0], [0,0,0,1/2,1/2,0,0], [0,0,0,0,0,1/2,1/2]]}
+
+param_bond_treasury_carry = {"m": 5, "lags": [50, 100, 150], "transform_matrix": [[1,0,0,0,0,0,0], [0,1,0,0,0,0,0], [0,0,1,0,0,0,0], [0,0,0,1/2,1/2,0,0], [0,0,0,0,0,1/3,2/3]]}
+transform_matrix, position_list, m=60, x=[7/365, 2, 4, 6, 17/2, 50/3], lags=[250, 500, 750]
+param_bond_finance_carry = {"m": 5, "lags": [50, 100, 150], "transform_matrix": [[1,0,0,0,0,0,0], [0,1,0,0,0,0,0], [0,0,1,0,0,0,0], [0,0,0,1/2,1/2,0,0], [0,0,0,0,0,1/3,2/3]]}
+param_bond_corporate_carry = {"m": 5, "lags": [50, 100, 150], "transform_matrix": [[1,0,0,0,0,0,0], [0,1,0,0,0,0,0], [0,0,1,0,0,0,0], [0,0,0,1/2,1/2,0,0], [0,0,0,0,0,1/2,1/2]]}
+
+
+
+
+
+
+param_asset_class_rev = {"lags": [240, 300, 360]}
+param_asset_class_vol = {"lags": [60, 120, 180]}
+param_asset_class_value = {"m": 5}
+
+
+
+
+
+
+
+
+
 
 
 
@@ -51,24 +118,15 @@ df_date = pd.read_csv(trade_days_file, parse_dates=[0], index_col=[0], header=[0
 order_date = df_date["20081231":"20180531"].iloc[::5, :]
 # 由order_date控制交易信号
 
-
-## 计算因子
-stock_size = pd.read_csv('stock_size.csv', index_col=[0], header=[0, 1], parse_dates=[0])
-stock_size_mom = stock_mom(stock_size)
-stock_size_vol = stock_vol(stock_size)
-
-stock_sector = pd.read_csv('stock_sector.csv', index_col=[0], header=[0, 1], parse_dates=[0])
-stock_sector_mom = stock_mom(stock_sector)
-stock_sector_vol = stock_vol(stock_sector)
-
-
-## 计算净值
-sr_rtn = net_value(stock_size, stock_size_mom)
-(sr_rtn + 1).cumprod().plot()
-sr_rtn = net_value(stock_size, stock_size_vol)
-(sr_rtn + 1).cumprod().plot()
-
-sr_rtn = net_value(stock_sector, stock_sector_mom, [1/14]*14 + [-1/14]*14)
-(sr_rtn + 1).cumprod().plot()
-sr_rtn = net_value(stock_sector, stock_sector_vol, [1/14]*14 + [-1/14]*14)
-(sr_rtn + 1).cumprod().plot()
+# 计算溢价策略表现，因子值要统一为越大越好
+def net_value(df_price, df_factor, position=[0.5, 0.5, -0.5, -0.5]):
+    price, factor = df_price["20081231":], df_factor["20081231":]
+    # T-1日末发出信号，T日以收盘价建仓
+    df_rank = factor.rank(axis=1, ascending=False, na_option="top")
+    df_position = df_rank.applymap(lambda x: position[int(x)-1])
+    # 从T+1日起收益开始计算
+    df_rtn = price.pct_change(1).fillna(0)
+    df_pos = df_position.loc[order_date.index, :]
+    df_pos = df_pos.reindex(df_rtn.index).fillna(method="bfill")
+    sr_rtn = (df_pos.shift(1) * df_rtn).sum(axis=1)
+    return sr_rtn
